ffe5186c40db54fdcc19afc0e78d26e5ae777303
bopzen <boppp@abv.bg>
2024-03-20 11:33:42 +0200

    changed text

--- src/views/catalog.js
+++ src/views/catalog.js
@@ -5,7 +5,7 @@ import { getAllBooks } from '../api/data.js';
 
 
 const catalogTemplate = (books) => html `<section id="dashboard-page" class="dashboard">
-    <h1>All Books</h1>
+    <h1>All Books Catalog</h1>
     <!-- Display ul: with list-items for All books (If any) -->
     ${books.length == 0 ? html`<p class="no-books">No books in database!</p>` : html` <ul class="other-books-list">
         ${books.map(bookTemplate)}</ul>`}
