2749fa18d2b6881f16cc37dae524ad9af9772c88
bopzen <boppp@abv.bg>
2024-03-20 09:05:52 +0200

    init pipeline branch

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -323,6 +323,6 @@ test('Verify redirection of Logout link after user login', async ({ page}) => {
     await logoutLink.click();
 
     const redirectedURL = page.url();
-    expect(redirectedURL).toBe('http://localhost:3000/catalog');
+    expect(redirectedURL).toBe('http://localhost:3000');
 });
 
